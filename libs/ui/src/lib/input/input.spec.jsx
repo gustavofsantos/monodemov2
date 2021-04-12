e9b4971847fa085a45dfcb21fea1ab99caa5b79c
Gustavo Santos <gustavofsantos@outlook.com>
2021-04-12 14:38:31 -0300

    added test runner

--- libs/ui/src/lib/input/input.spec.jsx
+++ libs/ui/src/lib/input/input.spec.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Input } from './input';
 
-describe('Input', () => {
+describe(Input.name, () => {
   it('should render the value and label', () => {
     render(<Input label="input label" value="the value" isReadOnly />);
 
